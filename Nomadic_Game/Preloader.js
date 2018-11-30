0c5b118d556bd4b22ea874e8af6254e21a04579c
katlego tshepho gaveni <31534028+katlego95@users.noreply.github.com>
2018-11-30 12:09:41 +0200

    Add files via upload

---
+++ Nomadic_Game/Preloader.js
@@ -0,0 +1,36 @@
+Game.Preloader = function (game){
+  this.preloadBar= null ;
+};
+
+Game.Preloader.prototype = {
+  preload: function(){
+    this.preloadBar = this.add.sprite(this.world.centerX,this.world.centerY,'preloaderBar');
+
+    this.preloadBar.anchor.setTo(0.5,0.5);
+    this.time.advancedTiming = true;
+    this.load.setPreloadSprite(this.preloadBar);
+
+
+    this.load.tilemap('map','assets/level1.csv');
+    this.load.image('tileset','assets/tileset.png');
+
+    this.load.spritesheet('player','assets/player.png',24,26);
+this.load.spritesheet('buttons','assets/buttons.png',193,71); // loading all assets
+this.load.image('drag','assets/drag.png');
+this.load.image('bird','assets/bird.png');
+this.load.image('nut','assets/nut.png');
+
+///////////////////////////////////////////////////
+this.load.image('star', 'assets/star.png');
+this.load.image('bomb', 'assets/bomb.png');
+//////////////////////////////////////////////////
+
+this.load.image('titlescreen','assets/titlescreen.png');
+this.load.image('button','assets/button.png');
+
+  },
+
+  create:function(){
+    this.state.start ('MainMenu'); // start main menu
+  }
+};
